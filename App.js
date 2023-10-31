76fdc464650d78e459d5f83be72f7b576576f704
pauloh-fm <phfarmar@gmail.com>
2023-10-31 00:41:38 -0300

    [FEAT] recriando o app.js

--- App.js
+++ App.js
@@ -1,20 +1,62 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
 
-export default function App() {
+const App = () => {
+  const [rating, setRating] = useState(null);
+
+  const handleRating = (value) => {
+    setRating(value);
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.heading}>Avalie o Atendimento</Text>
+      <View style={styles.ratingContainer}>
+        {[1, 2, 3, 4, 5].map((value) => (
+          <TouchableOpacity
+            key={value}
+            style={[styles.ratingButton, rating === value && styles.selectedRating]}
+            onPress={() => handleRating(value)}
+          >
+            <Text>{value}</Text>
+          </TouchableOpacity>
+        ))}
+      </View>
+      <Text style={styles.selectedRatingText}>Sua avaliação: {rating || 'Nenhuma'}</Text>
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    alignItems: 'center',
+  },
+  heading: {
+    fontSize: 20,
+    marginBottom: 20,
+  },
+  ratingContainer: {
+    flexDirection: 'row',
+  },
+  ratingButton: {
+    width: 40,
+    height: 40,
+    borderRadius: 20,
+    borderWidth: 1,
+    borderColor: 'gray',
+    justifyContent: 'center',
+    alignItems: 'center',
+    margin: 5,
+  },
+  selectedRating: {
+    borderColor: 'blue',
+  },
+  selectedRatingText: {
+    marginTop: 20,
+    fontSize: 16,
   },
 });
+
+export default App;
